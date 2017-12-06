aec529d6314ba1ee1eb14472590e62bf551829f0
company <372855309@qq.com>
2017-12-06 17:50:11 +0800

    no message

--- controllers/fileiovc.go
+++ controllers/fileiovc.go
@@ -12,12 +12,16 @@ type FileController struct {
 func (c *FileController) Get() {
 
 	// c.Data["cname"] = "www.zxw.com"
-	data := createTemp("999", "baidu.com", "www/sd/dd")
+	data := createTemp("999我去玩", "baidu.com", "www/sd/dd")
 	c.Data["path"] = data
 	fmt.Println(data)
 	c.TplName = "temp.tpl"
-	utils.WriteFile("/Users/zxw/www/aa.conf", data)
-
+	result := utils.WriteFile("/Users/zxw/www/aa.conf", data)
+	if result {
+		fmt.Println("网站配置创建成功")
+	} else {
+		fmt.Println("网站配置创建失败")
+	}
 }
 func createTemp(port string, name string, path string) string {
 	v := "server{\n    listen " + port + "; \n    server_name " + name + "; \n    root " + path + ";\n}"
